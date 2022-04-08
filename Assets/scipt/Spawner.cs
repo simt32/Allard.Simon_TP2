163e069cb68504578668ae7a2f445d00d30ccb3b
simt32 <simall2009@hotmail.com>
2022-04-07 20:34:53 -0400

    Placement
    les tourelle ce place au bonne endroit mais la fonctionaliter de les vendre ne fonctionne pas

--- Assets/scipt/Spawner.cs
+++ Assets/scipt/Spawner.cs
@@ -12,7 +12,7 @@ public class Spawner : MonoBehaviour
     
     
     private int nbr_ennemi;
-    int wave = 1;
+    int wave = 2;
     bool ispawn = true;
     private List<GameObject> listeEnnemi;
     // Start is called before the first frame update
@@ -42,13 +42,20 @@ public class Spawner : MonoBehaviour
 
     IEnumerator attendre()
     {
-        Enemi warrok = Instantiate(warrokPrefab, new Vector3(-11f, 0.6f, 57f), Quaternion.identity);
-        
-        yield return new WaitForSeconds(Random.Range(1f,6f));
-        Enemi nightshade = Instantiate(nightshadePrefab, new Vector3(-11f, 0.6f, 57f), Quaternion.identity);
-        
-        yield return new WaitForSeconds(Random.Range(3f, 9f));
-        Enemi squeltonzombie = Instantiate(squeltonzombieprefab, new Vector3(-11f, 0.6f, 57f), Quaternion.identity);
+        //yield return new WaitForSeconds(20f);
+        for (int i = 0; i < wave; i++)
+        {
+            Enemi warrok = Instantiate(warrokPrefab, new Vector3(-11f, 0.6f, 57f), Quaternion.identity);
+
+            yield return new WaitForSeconds(Random.Range(1f, 6f));
+            Enemi nightshade = Instantiate(nightshadePrefab, new Vector3(-11f, 0.6f, 57f), Quaternion.identity);
+
+            yield return new WaitForSeconds(Random.Range(3f, 9f));
+            Enemi squeltonzombie = Instantiate(squeltonzombieprefab, new Vector3(-11f, 0.6f, 57f), Quaternion.identity);
+
+        }
+
+            
        
 
 
--- Assets/scipt/turretSpawn.cs
+++ Assets/scipt/turretSpawn.cs
@@ -6,62 +6,86 @@ using UnityEngine.UI;
 public class turretSpawn : MonoBehaviour
 {
     public GameObject turretBasePrefab;
-    public GameObject turretPrefab;
-    public GameObject slowerPrefab;
+    public GameObject turret_Gun_Prefab;
+    public GameObject turret_Slower_Prefab;
+    public GameObject turret_Cannon_Prefab;
+    
+    GameObject turret_slower;
+    public GameObject cube;
     public Button slower;
     public Button cannon;
     public Button gun;
+    public Button cancel;
+    public Button vendre;
     public Transform Base;
     private GameObject test;
+    private bool click = true;
+    private bool btn_VendreIsActive = false;
    
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("test");
-        
+
         slower.onClick.AddListener(btn_Slower_onClicked);
+        cannon.onClick.AddListener(btn_Gun_onClicked);
+        gun.onClick.AddListener(btn_Cannon_onClicked);
+        cancel.onClick.AddListener(btn_Cancel_onClicked);
+        vendre.onClick.AddListener(btn_Vendre_onClicked);
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        if (Input.GetMouseButtonDown(0))
+        if (click)
         {
-            RaycastHit raycastHit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if (Physics.Raycast(ray, out raycastHit, 100f))
+            if (Input.GetMouseButtonDown(0))
             {
-                if (raycastHit.transform != null)
+                RaycastHit raycastHit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out raycastHit, 100f))
                 {
+                    if (raycastHit.transform != null)
+                    {
 
-                    
-                    test = raycastHit.transform.gameObject;
-                    Debug.Log(raycastHit.transform.gameObject);
-                    CurrentClickedGameObject(raycastHit.transform.gameObject);
-                    
-                    
+
+                        test = raycastHit.transform.gameObject;
+                        Debug.Log(test);
+                        //Debug.Log(raycastHit.transform.gameObject);
+                        CurrentClickedGameObject(raycastHit.transform.gameObject);
+
+
+                    }
                 }
             }
         }
+        
 
     }
     public void CurrentClickedGameObject(GameObject gameObject)
     {
-        
+        //if (gameObject.tag == "occuper")
+        //{
+        //    vendre.gameObject.SetActive(true);
+        //    cancel.gameObject.SetActive(true);
+        //    btn_VendreIsActive = true;
+        //    test = gameObject;
+        //}
         if (gameObject.tag == "TurretBase")
         {
             test = gameObject;
+
             slower.gameObject.SetActive(true);
             cannon.gameObject.SetActive(true);
             gun.gameObject.SetActive(true);
-            Debug.Log(gameObject);
-            Debug.Log("sa marche");
-            //Instantiate(turretPrefab, gameObject.transform.position,new Quaternion(-90f,0f,0f,90f));
-            Debug.Log(gameObject.transform.position);
-            gameObject.tag = "occuper";
+            cancel.gameObject.SetActive(true);
+           
+            //gameObject.tag = "occuper";
+            click = false;
 
         }
+        
     }
 
 
@@ -69,8 +93,86 @@ public class turretSpawn : MonoBehaviour
     {
         
         Debug.Log("bouton click");
-        Instantiate(slowerPrefab, test.transform.position, new Quaternion(-90f, 0f, 0f, 90f));
-        Debug.Log(test.transform.position);
+        turret_slower = Instantiate(turret_Slower_Prefab, test.transform.position, new Quaternion(-90f, 0f, 0f, 90f));
+        //Debug.Log(test.transform.position);
+        slower.gameObject.SetActive(false);
+        cannon.gameObject.SetActive(false);
+        gun.gameObject.SetActive(false);
+        cancel.gameObject.SetActive(false);
+        test.tag = "occuper";
+        
+        click = true;
         
     }
+    void btn_Gun_onClicked()
+    {
+
+        Debug.Log("bouton click");
+        Instantiate(turret_Gun_Prefab, test.transform.position, new Quaternion(-90f, 0f, 0f, 90f));
+        //Debug.Log(test.transform.position);
+        slower.gameObject.SetActive(false);
+        cannon.gameObject.SetActive(false);
+        gun.gameObject.SetActive(false);
+        cancel.gameObject.SetActive(false);
+        test.tag = "occuper";
+
+        click = true;
+
+    }
+
+    void btn_Cannon_onClicked()
+    {
+
+        Debug.Log("bouton click");
+        Instantiate(turret_Cannon_Prefab, test.transform.position, new Quaternion(-90f, 0f, 0f, 90f));
+        //Debug.Log(test.transform.position);
+        slower.gameObject.SetActive(false);
+        cannon.gameObject.SetActive(false);
+        gun.gameObject.SetActive(false);
+        cancel.gameObject.SetActive(false);
+        test.tag = "occuper";
+
+        click = true;
+
+    }
+    void btn_Cancel_onClicked()
+    {
+        //Debug.Log("bouton click");
+        //if (btn_VendreIsActive)
+        //{
+        //    test.tag = "occuper";
+        //}
+        //Debug.Log(test.transform.position);
+        slower.gameObject.SetActive(false);
+        cannon.gameObject.SetActive(false);
+        gun.gameObject.SetActive(false);
+        cancel.gameObject.SetActive(false);
+        vendre.gameObject.SetActive(false);
+        test.tag = "TurretBase";
+        //if (!btn_VendreIsActive)
+        //{
+        //    test.tag = "TurretBase";
+        //}
+        
+        click = true;
+
+    }
+    void btn_Vendre_onClicked()
+    {
+        Debug.Log("bouton click");
+
+        //Debug.Log(test.transform.position);
+        cancel.gameObject.SetActive(false);
+        vendre.gameObject.SetActive(false);
+        
+        
+        Debug.Log(test.tag);
+        Destroy(turret_slower);
+        
+        click = true;
+        btn_VendreIsActive = false;
+        test.tag = "TurretBase";
+
+    }
+
 }
